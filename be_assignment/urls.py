9f948df4b831aac5909ed00d76a0e57baea354b6
Soham Sagathiya <sagathiyasoham12345@gmail.com>
2026-03-14 10:20:16 +0530

    done

--- be_assignment/urls.py
+++ be_assignment/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import ArticleListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/articles/', ArticleListView.as_view(), name='article-list'),
 ]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now, db_index=True)
+    is_published = models.BooleanField(default=False, db_index=True)
+
+    class Meta:
+        ordering = ['created_at', 'id']
+
+    def __str__(self):
+        return self.title
