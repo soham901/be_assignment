b67e181ee7a8bd3d0e198b76bcf9db5591e76032
Soham Sagathiya <sagathiyasoham12345@gmail.com>
2026-03-14 10:35:47 +0530

    task 2

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from core.models import Article
+
+
+class ArticlePaginationTests(APITestCase):
+    fixtures = ['articles.json']
+
+    def test_article_list_uses_global_default_pagination(self):
+        response = self.client.get(reverse('article-list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 15)
+
+    def test_article_list_honors_page_size_query_param(self):
+        response = self.client.get(reverse('article-list'), {'page_size': 5})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 5)
+
+    def test_article_list_caps_page_size_at_max_limit(self):
+        Article.objects.bulk_create(
+            [
+                Article(
+                    title=f'Extra Article {index}',
+                    content='Extra content',
+                    author='Load Test',
+                    is_published=True,
+                )
+                for index in range(1, 31)
+            ]
+        )
+
+        response = self.client.get(reverse('article-list'), {'page_size': 100})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 50)
+
+    def test_invalid_page_size_falls_back_to_default_page_size(self):
+        response = self.client.get(reverse('article-list'), {'page_size': 'abc'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 15)
--- core/views.py
+++ core/views.py
@@ -1,17 +1,11 @@
 from rest_framework import generics
-from rest_framework.pagination import PageNumberPagination
 
 from core.models import Article
 from core.serializers import ArticleSerializer
 
 
-class ArticlePagination(PageNumberPagination):
-    page_size = 10
-
-
 class ArticleListView(generics.ListAPIView):
     serializer_class = ArticleSerializer
-    pagination_class = ArticlePagination
 
     def get_queryset(self):
         return Article.objects.filter(is_published=True).order_by('created_at', 'id')
